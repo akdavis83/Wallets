7d727efbd4d8611de963c927b83284806d1b5938
Alan Davis <155010643+akdavis83@users.noreply.github.com>
2024-08-24 15:05:57 -0400

    Update save-simple-wallet.js

--- save-simple-wallet.js
+++ save-simple-wallet.js
@@ -8,7 +8,7 @@ async function saveWalletToJSON(wallet, password) {
     return wallet.encrypt(password)
 }
 
-let privateKey = '0x495d5c34c912291807c25d5e8300d20b749f6be44a178d5c50f167d495f3315a'
+let privateKey = '0x'
 
 let wallet = recoverWalletFromPrivateKey(privateKey)
 let password = 'p@ssw0rd~3';
